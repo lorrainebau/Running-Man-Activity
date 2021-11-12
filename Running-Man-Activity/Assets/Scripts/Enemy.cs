2b458eef7a358d515b6c5f46e334db1803c55c0b
Bautista, Angelica Lorraine L <88190150+lorrainebau@users.noreply.github.com>
2021-11-12 13:55:45 +0800

    Uploaded the activity Running Man

---
+++ Running-Man-Activity/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject target; //player
+    UnityEngine.AI.NavMeshAgent agent; // enemy
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if(target == null)
+            target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this is for updating the target
+        agent.destination = target.transform.position;
+    }
+    //function for catch player
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        SceneManager.LoadScene("SampleScene");
+    }
+}
